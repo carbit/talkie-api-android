82741d2463c9756d0f1f02fb180bf9178ea690c0
liaoyb <45554592@qq.com>
2017-03-07 10:40:32 +0800

    SDK 1.1

--- TalkieSDK_Demo/app/src/main/java/net/easyconn/sdk/talkie/demo/MainActivity.java
+++ TalkieSDK_Demo/app/src/main/java/net/easyconn/sdk/talkie/demo/MainActivity.java
@@ -2,7 +2,9 @@ package net.easyconn.sdk.talkie.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,11 +17,14 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView txRegist = null;
     private TextView txOnline = null;
+    private EditText mIdEditText;
     private TextView txReqSpeak = null;
     private TextView txStopSpeak = null;
     private TextView txOtherSpeak = null;
     private EditText edRegist = null;
     private boolean isClickStop = false;
+
+    private TextView mTvLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,15 +41,17 @@ public class MainActivity extends AppCompatActivity {
     private void initView() {
         txRegist = (TextView) findViewById(R.id.tx_regist);
         txOnline = (TextView) findViewById(R.id.tx_online);
+        mIdEditText = (EditText) findViewById(R.id.ed_id);
         txReqSpeak = (TextView) findViewById(R.id.tx_req_speak);
         txStopSpeak = (TextView) findViewById(R.id.tx_stop_speak);
         txOtherSpeak = (TextView) findViewById(R.id.tx_other_speak);
         edRegist = (EditText) findViewById(R.id.ed_regist);
+        mTvLocation = (TextView) findViewById(R.id.tv_location);
 
         findViewById(R.id.regist).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TalkieManager.login("", edRegist.getText().toString(), new TalkieClient.ConnectCallback() {
+                TalkieManager.login("", edRegist.getText().toString().trim(), new TalkieClient.ConnectCallback() {
                     @Override
                     public void onSuccess(String openid) {
                         showToast("登录成功");
@@ -63,11 +70,24 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.online).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TalkieManager.online("1", new TalkieClient.OnlineCallback() {
+                String id = mIdEditText.getText().toString().trim();
+                if (TextUtils.isEmpty(id)) {
+                    showToast("id不能为空");
+                    return;
+                }
+                if (!id.matches("\\d{1,5}")) {
+                    showToast("id必须为5位内数字");
+                    return;
+                }
+
+                TalkieManager.online(id, new TalkieClient.OnlineCallback() {
                     @Override
                     public void onSuccess() {
                         showToast("加入成功");
                         txOnline.setText("加入成功");
+
+                        InputMethodManager manager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                        manager.hideSoftInputFromWindow(mIdEditText.getWindowToken(), 0);
                     }
 
                     @Override
@@ -127,6 +147,21 @@ public class MainActivity extends AppCompatActivity {
                     txReqSpeak.setText("抢麦超过30秒，服务器通知丢麦");
                 }
                 isClickStop = false;
+            }
+        });
+
+        findViewById(R.id.btn_location).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TalkieManager.location(30.574305D, 114.286212D, 10, 30);
+                mTvLocation.setText("位置上报 纬度:30.574305 经度:114.286212");
+            }
+        });
+
+        TalkieManager.setOtherLocationListener(new TalkieClient.LocationListener() {
+            @Override
+            public void onLocationListener(String openid, double lat, double lon, int speed, int direction) {
+                mTvLocation.setText(String.format("%s位置变更纬度:%s 经度:%s", openid, lat, lon));
             }
         });
     }
