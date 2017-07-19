7bc8645ad0c4983dc3857aab726724522d8dec91
young <young.liang@carbit.com.cn>
2017-07-19 10:17:47 +0800

    更新SDK 添加sdkVersion buildNumber

--- TalkieSDK_Demo/app/src/main/java/net/easyconn/sdk/talkie/demo/MainActivity.java
+++ TalkieSDK_Demo/app/src/main/java/net/easyconn/sdk/talkie/demo/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -44,6 +45,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        Log.e("TalkieSDK", "SdkVersion:" + TalkieManager.getSdkVersion());
+        Log.e("TalkieSDK", "BuildNumber:" + TalkieManager.getBuildNumber());
 
         initView();
         initListener();
